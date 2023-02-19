14abaf68f9e115bc8712e3acf9505588578f8010
Michael Shamberger <ext-michael.shamberger@finago.com>
2023-02-19 14:21:46 +0200

    add some i8n

--- mcweb/urls.py
+++ mcweb/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include, re_path
 from django.contrib import admin
-
 from backend.version import version
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('', include('frontend.urls')), 
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin', admin.site.urls),
     path('api/auth/', include('backend.users.urls')),
     path('api/search/', include('backend.search.urls')),
